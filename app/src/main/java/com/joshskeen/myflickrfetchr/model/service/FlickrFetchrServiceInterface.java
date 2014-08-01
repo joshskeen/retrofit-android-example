7d0e1adc95f8c4589b98a948ec98e73b911f8573
Josh Skeen <josh@joshskeen.com>
2014-08-01 15:25:49 -0700

    adds a comment about the async interface

--- app/src/main/java/com/joshskeen/myflickrfetchr/model/service/FlickrFetchrServiceInterface.java
+++ app/src/main/java/com/joshskeen/myflickrfetchr/model/service/FlickrFetchrServiceInterface.java
@@ -14,6 +14,7 @@ public interface FlickrFetchrServiceInterface {
     @GET("/services/rest/?method=flickr.photos.getRecent")
     public GetRecentPhotosResponse getRecentPhotos(@Query("per_page") int perPage, @Query("page") int page);
 
+    //uses the asynchronous callback interface Retrofit provides out of the box
     @GET("/services/rest/?method=flickr.photos.getRecent")
     public void asyncGetRecentPhotos(@Query("per_page") int perPage, @Query("page") int page, Callback<GetRecentPhotosResponse> callback);
 
